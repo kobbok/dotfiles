07aff479578a97243ac227ccb67061ac35090053
kobbok <76209741+kobbok@users.noreply.github.com>
2024-10-08 11:46:13 +0200

    neovim - Disable bufferlinevim plugin

---
+++ nvim-config/lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  -- disable bufferlinevim, I have no idea why you would want this it cluters everything so much, especially if you're not used to closing buffers, tabs should be tabs not files...
+  { "akinsho/bufferline.nvim", enabled = false },
+}
