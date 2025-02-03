90e9a1e45aa8b9d8ed23451659de5950666124dc
kobbok <76209741+kobbok@users.noreply.github.com>
2025-02-03 18:09:02 +0100

    git - Enable fetch.prune globally

--- shovel-shit-macos.sh
+++ shovel-shit-macos.sh
@@ -3,3 +3,5 @@ mkdir -p ~/.config/nvim/ && cp -r nvim-config ~/.config/nvim/
 mkdir -p ~/.config/kitty/ && cp kitty.conf ~/.config/kitty/
 
 cp .zprofile .zshrc ~
+
+git config --global fetch.prune true
